f4a7e2cce7a5c7286616ec9cbe5be9388502c37f
mohabash011 <mohabashuni@gmail.com>
2024-10-31 19:47:50 +0300

    solved the entire lab in full

--- src/Document/Document/PublishingSystem.java
+++ src/Document/Document/PublishingSystem.java
@@ -2,17 +2,14 @@ package Document;
 
 public class PublishingSystem {
     public static void main(String[] args) {
-        // Create prototype
         DocumentTemplate prototype = new ArticleTemplate();
 
-        // Clone and customize first article
         DocumentTemplate article1 = prototype.clone();
         article1.setContent("Science Article");
         article1.setLayout("Two Column");
         article1.setStyle("Academic");
         article1.customize();
 
-        // Clone and customize second article
         DocumentTemplate article2 = prototype.clone();
         article2.setContent("News Article");
         article2.setLayout("Single Column");
